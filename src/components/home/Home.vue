8959b430e1641f4c70050e327d0b53e13b0ad03e
Guilherme <guilherme.salviano@hotmail.com>
2022-01-19 15:17:35 -0300

    beginning of the project

---
+++ src/components/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <navigation></navigation>
+    </div>
+</template>
+<script>
+import Navigation from "../shared/navigation/Navigation.vue"
+export default {
+    components:{
+        Navigation
+    }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/shared/navigation/Navigation.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navigation">
+        este é o site da Stephania
+    </nav>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+
+</style>
